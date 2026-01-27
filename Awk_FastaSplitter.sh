ea5c38e1be8a9e78ed0b420260f978ab925395c7
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 15:15:34 -0800

    Update and rename AwkSplitter.sh to Awk_FastaSplitter.sh

--- Awk_FastaSplitter.sh
+++ Awk_FastaSplitter.sh
@@ -5,6 +5,7 @@
 #SBATCH --cpus-per-task=15 ##number of cores to use per node
 #SBATCH -error=slurm-%J.err ##Error file
 
+#Below, awk splits a fasta into 15,000,000 basepair chunks
 
 awk 'BEGIN {n=0;} /^>/ {if(n%15000000==0){file=sprintf("chunk%d.fa",n);} print >> file; n++; next;} { print >> file; }' < multi.fa
 
