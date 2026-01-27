6c380fc55e02b507db079453430defd0fd9ca537
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 15:33:52 -0800

    Update and rename BamToBed.sh to Bedtools2_BamToBed.sh

--- Bedtools2_BamToBed.sh
+++ Bedtools2_BamToBed.sh
@@ -6,11 +6,9 @@
 #SBATCH --cpus-per-task=10 ##number of cores to use per node
 #SBATCH -error=slurm-%J.err ##Error file
 
-
 module load bedtools2
 
 BAM_INPUT=/dfs10/bio/clarklh/New_ChIP/Input_G_rmdup.bam
 BED_OUTPUT=/dfs10/bio/clarklh/New_ChIP/Input_G_rmdup.bed
 
 bedtools bamtobed -i $BAM_INPUT > $BED_OUTPUT
-
