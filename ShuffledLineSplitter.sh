29cc5253acb449431336169db48973bc247b8e57
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 17:00:48 -0800

    Update and rename Shplittuh.sh to ShuffledLineSplitter.sh

--- ShuffledLineSplitter.sh
+++ ShuffledLineSplitter.sh
@@ -5,7 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10 
 
-
 SAMPLE=/dfs7/mbt/aminaoh/my_data/ChIP-seq/2022-04-10-ep300-stage-10-5/Animal_Input/AC_input_RMDUP.bed
+OUT_PREFIX=/dfs7/mbt/aminaoh/my_data/ChIP-seq/2022-04-10-ep300-stage-10-5/Animal_Input/AC_input_chunk_
 
-cat $SAMPLE |shuf|split -d -l 10000000 > 01
+# Shuffle and split into 10 million lines each
+shuf "$SAMPLE" | split -d -l 10000000 --additional-suffix=.bed - "$OUT_PREFIX"
