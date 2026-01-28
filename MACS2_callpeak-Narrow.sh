5294d5ee62994fb4c04cd9140ceb6fd829f1246f
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 17:02:22 -0800

    Update and rename MacDaddy2.sh to MACS2_callpeak-Narrow.sh

--- MACS2_callpeak-Narrow.sh
+++ MACS2_callpeak-Narrow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A kwcho_lab
-#SBATCH -J MACS2_Sox3_PPseudo1
+#SBATCH -J MACS2
 #SBATCH -p standard
 #SBATCH --nodes=1 ##number of nodes to use
 #SBATCH --cpus-per-task=10 ##number of cores to use per node
@@ -14,5 +14,3 @@ INPUT=/dfs10/bio/clarklh/FoxH1_ChIP/RmDup_Reads/V10_0-Analysis/FoxH1_Inp_St105.s
 OUTPUT=/dfs10/bio/clarklh/New_ChIP/MACS2/Grhl1_ChIP_R1
 
 macs2 callpeak -t $CHIP -c $INPUT -n $OUTPUT -f BED -g 1.1e9 -p .001
-
-
