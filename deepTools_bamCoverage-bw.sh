486d42e24ffbbf98cd983c66b74527740b854cbc
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 16:34:49 -0800

    Update and rename DeepTools.sh to deepTools_bamCoverage-bw.sh

--- deepTools_bamCoverage-bw.sh
+++ deepTools_bamCoverage-bw.sh
@@ -8,15 +8,13 @@
 
 #DeepTools-bamCoverage is housed within anaconda, and is therefore loaded alongside anaconda.
 #-b = your input bam file to convert into a bw (BigWig) file.
-#-o = your output bw (BigWig) file containing coverage information.
+#-o = your output bw (BigWig) file containing coverage information. 
 
-#--normalizeUsing RPKM --normalizeUsing RPKM 
-
+module load anaconda
 module load deeptools
 
 BAM_READS=/dfs10/bio/clarklh/EP300_ChIP/FS_MO/Aligned/P300_FSmo_St105_AC_Pooled_rmdup_sorted.bam
 OUTPUT=/dfs10/bio/clarklh/EP300_ChIP/FS_MO/Aligned/P300_FSmo_St105_AC_Pooled_rmdup_sorted.bw
+SCALE_FACTOR=0.81
 
-bamCoverage -b $BAM_READS  -o $OUTPUT  --binSize 20 --effectiveGenomeSize 1100000000 --normalizeUsing RPKM  #--scaleFactor .50
-
-#--scaleFactor .81
+bamCoverage -b $BAM_READS  -o $OUTPUT  --binSize 20 --effectiveGenomeSize 1100000000 --normalizeUsing RPKM --scaleFactor $SCALE_FACTOR
