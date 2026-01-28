8a00efecad36b71d1f31cac0bd47edce5d170631
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 17:19:47 -0800

    Update and rename ToolsOfTheBed.sh to Bedtools2_intersect-overlappingFeatures.sh

--- Bedtools2_intersect-overlappingFeatures.sh
+++ Bedtools2_intersect-overlappingFeatures.sh
@@ -7,7 +7,6 @@
 #SBATCH -error=slurm-%J.err ##Error file
 #SBATCH -t 12:00:00
 
-
 #bedtools intersect command is really useful for filtering similarities, or dissimilarities, between .bed format files. The main use is filtering for physically overlapping peaks to create logical Venn Diagrams. Another use is for filtering .summit files using previous filtered IDR.narrowPeak files. This is useful because in order to call Motifs on persistent peaks, we need to define the persistent peaks by intersecting IDR outputs. Next we need to filter our Pooled.summit file using our intersected/filtered IDR output to create a summit files containing only persistent peak summits. Next we use bedtools slop to create a new bedfile containing the peak summit plus (x) amount of bases on either side of the summit. If our slop usage is set to 50bp, then our output slop.bed file will contain the peak summit, along with 50bp on either side of the summit, giving a 101bp peak fragment, centered on the summit. Very finally we use getfasta command on the slop output to create fasta files of the 101 bp fragment centered on summit. MeMe-ChIP uses these fasta fragments centered on summit to then call Motif enrichment across the particular population on peaks.
 
 module load bedtools2
@@ -20,9 +19,3 @@ bedtools intersect -wa\
  -a $QUERY \
  -b $TARGET \
  > $OUTPUT
-
-#/dfs5/bio/clarklh/Foxi2_ChIP/MACS2-IDR/Bowtie_V10/IDR/St105/Foxi2_St105_R1vR2_IDR.IDR0.05.narrowPeak
-
-#/dfs7/mbt/aminaoh/my_data/ChIP-seq/2022-04-10-ep300-stage-10-5/Animal_ChIP/IDR/ep300_AC_stage10_5_pooled_r1vsr2.IDR0.05.sorted.narrowPeak
-
-#/dfs5/bio/clarklh/Sox3_ChIP/V10_Intersect/P300/Sox3_St89105Combined_P300-AC_Peaks-wa_sorted.narrowPeak
