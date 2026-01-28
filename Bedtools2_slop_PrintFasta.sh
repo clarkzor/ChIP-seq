75857717cb0e1f591fb620aa321289175d2ef258
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 17:26:51 -0800

    Update Bedtools2_slop_PrintFasta.sh

--- Bedtools2_slop_PrintFasta.sh
+++ Bedtools2_slop_PrintFasta.sh
@@ -12,5 +12,6 @@ module load bedtools2
 SUMMITS=/dfs7/mbt/aminaoh/my_data/ChIP-seq/2022-04-10-ep300-stage-10-5/Animal_ChIP/macs2_analysis/ep300_AC_stage10_5_pooled_r1vsr2.IDR0.05.sorted_summits.narrowPeak
 GENOME=/dfs5/bio/clarklh/RNA_Seq/ReferenceGenome/XtropV10.genome
 OUTPUT=/dfs7/mbt/aminaoh/my_data/ChIP-seq/2022-04-10-ep300-stage-10-5/Animal_ChIP/macs2_analysis/ep300_AC_stage10_5_pooled_r1vsr2.IDR0.05.sorted_summits_slop250.narrowPeak
+COORDINATE_WIDTH_TO_EXTEND_FROM_SUMMIT=250
 
-bedtools slop -i $SUMMITS -g $GENOME -b 250 > $OUTPUT
+bedtools slop -i $SUMMITS -g $GENOME -b $COORDINATE_WIDTH_TO_EXTEND_FROM_SUMMIT > $OUTPUT
